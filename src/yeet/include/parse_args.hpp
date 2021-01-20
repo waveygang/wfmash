23f24407c0e6d83cacdd80434bf2085029cfd9ae
Erik Garrison <erik.garrison@gmail.com>
2021-01-20 13:44:48 +0100

    try again

--- src/yeet/include/parse_args.hpp
+++ src/yeet/include/parse_args.hpp
@@ -144,7 +144,7 @@ void parse_args(int argc,
     }
 
     if (map_pct_identity) {
-        if (map_parameters.percentageIdentity < 70) {
+        if (args::get(map_pct_identity) < 70) {
             std::cerr << "[wfmash] ERROR, skch::parseandSave, minimum nucleotide identity requirement should be >= 70\%" << std::endl;
             exit(1);
         }
