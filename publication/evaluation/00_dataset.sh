6729969b68897b411788bf6f08f1021e8a359ad0
AndreaGuarracino <andreaguarracino@outlook.com>
2021-04-05 10:25:09 +0200

    cleaning

--- publication/evaluation/00_dataset.sh
+++ publication/evaluation/00_dataset.sh
@@ -8,12 +8,13 @@ gunzip chm13.draft_v1.0.fasta.gz
 samtools faidx chm13.draft_v1.0.fasta
 samtools faidx chm13.draft_v1.0.fasta $(grep chr8 chm13.draft_v1.0.fasta.fai | cut -f 1) > chr8.fa
 
-# Simulate [18000, 22000] kbps reads
+# HiFi read simulation
+# [18000, 22000] kbps reads
 ~/git/PBSIM-PacBio-Simulator/src/pbsim --depth 20 --data-type CLR --accuracy-mean 0.999 --accuracy-min 0.99 --length-min 18000 --length-mean 20000 --length-max 22000 --model_qc ~/git/PBSIM-PacBio-Simulator/data/model_qc_clr --prefix chr8 chr8.fa
 sed -n '1~4s/^@/>/p;2~4p' chr8_0001.fastq > chr8.18_22kbps.fasta
 rm chr8_0001.ref chr8_0001.fastq
 
-# Simulate 1 Mbps reads
+# 1 Mbps reads
 ~/git/PBSIM-PacBio-Simulator/src/pbsim --depth 20 --data-type CLR --accuracy-mean 0.999 --accuracy-min 0.99 --length-min 1000000 --length-mean 1000000 --length-max 1000000 --model_qc ~/git/PBSIM-PacBio-Simulator/data/model_qc_clr --prefix chr8_1Mbps chr8.fa
 sed -n '1~4s/^@/>/p;2~4p' chr8_1Mbps_0001.fastq > chr8.1Mbps.fasta
 rm chr8_1Mbps8_0001.ref chr8_1Mbps_0001.fastq
--- publication/evaluation/01_alignment.sh
+++ publication/evaluation/01_alignment.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# Exit when any command fails
+set -eo pipefail
+
 threads=16
 
 prefix=chr8.18_22kbps
-/usr/bin/time -v wfmash chr8.fasta $prefix.fasta -t $threads -Na -s 1000 >$prefix.s1000.sam && samtools view $prefix.s1000.sam -bS -@ $threads | samtools sort -@ $threads >$prefix.s1000.bam && samtools index $prefix.s1000.bam && samtools coverage $prefix.s1000.bam
+/usr/bin/time -v wfmash chr8.fasta $prefix.fasta -t $threads -Na -s 1000 >$prefix.s1000.sam
+bash sam2bam.sh $prefix.s1000.sam -@ $threads
 
-/usr/bin/time -v minimap2 chr8.fasta $prefix.fasta-t $threads -ca >$prefix.mp2.sam && samtools view $prefix.mp2.sam -bS -@ $threads | samtools sort -@ $threads >$prefix.mp2.bam && samtools index $prefix.mp2.bam
+/usr/bin/time -v minimap2 chr8.fasta $prefix.fasta -t $threads -ca -x asm20 >$prefix.mp2.sam
+bash sam2bam.sh $prefix.mp2.sam -@ $threads
 
 prefix=chr8_1Mbps
-/usr/bin/time -v wfmash chr8.fasta $prefix.fasta -t $threads -Na -s 50000 >$prefix.s50000.sam && samtools view $prefix.s50000.sam -bS -@ $threads | samtools sort -@ $threads >$prefix.s50000.bam && samtools index $prefix.s50000.bam && samtools coverage $prefix.s50000.bam
+/usr/bin/time -v wfmash chr8.fasta $prefix.fasta -t $threads -Na -s 50000 >$prefix.s50000.sam
+bash sam2bam.sh $prefix.s50000.sam -@ $threads
 
-/usr/bin/time -v minimap2 chr8.fasta $prefix.fasta -t $threads -ca >$prefix.mp2.sam && samtools view $prefix.mp2.sam -bS -@ $threads | samtools sort -@ $threads >$prefix.mp2.bam && samtools index $prefix.mp2.bam
+/usr/bin/time -v minimap2 chr8.fasta $prefix.fasta -t $threads -ca -x asm20 >$prefix.mp2.sam
+bash sam2bam.sh $prefix.mp2.sam -@ $threads
