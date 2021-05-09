8e691860a25c20f7aaeaeb4bca1aa26312b76c76
AndreaGuarracino <andreaguarracino@outlook.com>
2021-05-09 23:30:24 +0200

    all in

--- src/map/include/map_stats.hpp
+++ src/map/include/map_stats.hpp
@@ -243,7 +243,8 @@ namespace skch
         }
       }
 
-      int w =  (2.0 * lengthQuery)/optimalSketchSize;
+      //int w =  (2.0 * lengthQuery)/optimalSketchSize;
+      int w =  (0.5 * lengthQuery)/optimalSketchSize; // harder window size
 
       // 1 <= w <= lengthQuery
       return std::min( std::max(w,1), lengthQuery);
