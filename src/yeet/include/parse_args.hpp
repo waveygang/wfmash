2543c9cbe58516d5add19ec57d342f53ecb44b7c
AndreaGuarracino <andreaguarracino@outlook.com>
2021-05-28 11:39:58 +0200

    downsample less to get more and better mappings

--- src/yeet/include/parse_args.hpp
+++ src/yeet/include/parse_args.hpp
@@ -329,7 +329,7 @@ void parse_args(int argc,
                     map_parameters.percentageIdentity,
                     map_parameters.segLength,
                     map_parameters.referenceSize);
-            map_parameters.windowSize = std::min(256, map_parameters.windowSize);
+            map_parameters.windowSize = std::min(64, map_parameters.windowSize);
         }
     }
 
