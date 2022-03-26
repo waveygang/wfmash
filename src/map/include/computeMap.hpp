5fd9992641abecf20c7da01d9b410cad1ed2d3a4
Erik Garrison <erik.garrison@gmail.com>
2022-03-26 23:51:29 +0100

    allow merging mappings that partly overlap

--- src/map/include/computeMap.hpp
+++ src/map/include/computeMap.hpp
@@ -1024,11 +1024,11 @@ namespace skch
                       int query_dist = 0;
                       auto dist = std::numeric_limits<double>::max();
                       auto score = std::numeric_limits<double>::max();
-                      if (it->strand == strnd::FWD && it->queryEndPos <= it2->queryStartPos) {
+                      if (it->strand == strnd::FWD && it->queryStartPos <= it2->queryStartPos) {
                           query_dist = it2->queryStartPos - it->queryEndPos;
                           dist = std::sqrt(std::pow(query_dist,2) + std::pow(ref_dist,2));
                           score = std::pow(query_dist - ref_dist, 2);
-                      } else if (it->strand != strnd::FWD && it->queryStartPos >= it2->queryEndPos) {
+                      } else if (it->strand != strnd::FWD && it->queryEndPos >= it2->queryEndPos) {
                           query_dist = it->queryStartPos - it2->queryEndPos;
                           dist = std::sqrt(std::pow(query_dist,2) + std::pow(ref_dist,2));
                           score = std::pow(query_dist - ref_dist, 2);
