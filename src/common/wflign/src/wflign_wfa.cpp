9ad6aafdd85160716f40d35965ce4ba41eadf4ac
Erik Garrison <erik.garrison@gmail.com>
2022-06-02 14:02:52 +0200

    go minimal with the reduction distance

--- src/common/wflign/src/wflign_wfa.cpp
+++ src/common/wflign/src/wflign_wfa.cpp
@@ -331,7 +331,7 @@ void wflign_affine_wavefront(
 
         uint64_t wflambda_max_distance_threshold =
             std::min((uint64_t)std::max(query_length,target_length)/10,
-                std::max((uint64_t)_wflambda_max_distance_threshold,
+                std::min((uint64_t)_wflambda_max_distance_threshold,
                          (uint64_t)std::abs((int64_t)query_length-(int64_t)target_length))) / step_size;
 
         if (wflambda_min_wavefront_length || wflambda_max_distance_threshold) {
