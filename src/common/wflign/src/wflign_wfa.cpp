983e95f078175298574f336c25f0d37590587f7f
Erik Garrison <erik.garrison@gmail.com>
2022-06-02 13:30:34 +0200

    a less extreme minimum reduction distance for short seqs

--- src/common/wflign/src/wflign_wfa.cpp
+++ src/common/wflign/src/wflign_wfa.cpp
@@ -330,7 +330,7 @@ void wflign_affine_wavefront(
         // attributes.affine2p_penalties = affine2p_penalties;
 
         uint64_t wflambda_max_distance_threshold =
-            std::min((uint64_t)std::max(query_length,target_length)/5,
+            std::min((uint64_t)std::max(query_length,target_length)/10,
                 std::max((uint64_t)_wflambda_max_distance_threshold,
                          (uint64_t)std::abs((int64_t)query_length-(int64_t)target_length))) / step_size;
 
