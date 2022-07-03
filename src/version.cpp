0897c97f80478ca6ccae773ffabcbbc8dc4d73a5
Erik Garrison <erik.garrison@gmail.com>
2022-07-03 10:19:05 +0200

    oh comma

--- src/version.cpp
+++ src/version.cpp
@@ -44,7 +44,7 @@ namespace wfmash {
             {"v0.7.0", "Educazione"},
             {"v0.8.0", "pensiero divergente"},
             {"v0.8.1", "Divergenza"},
-            {"v0.8.2", "Pasticcione"}
+            {"v0.8.2", "Pasticcione"},
             {"v0.9.0", "Mutamento"}
             // Add more codenames here
     };
