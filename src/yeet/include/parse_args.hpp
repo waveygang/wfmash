5cb99aabb26416f9300e27d674ce40ac6ad82252
AndreaGuarracino <andreaguarracino@outlook.com>
2022-07-17 09:52:48 +0200

    explain a bit better `-Y/--skip-prefix` [skip ci]

--- src/yeet/include/parse_args.hpp
+++ src/yeet/include/parse_args.hpp
@@ -72,7 +72,7 @@ void parse_args(int argc,
     args::ValueFlag<uint32_t> num_mappings_for_segments(parser, "N", "number of mappings to retain for each segment [default: 1]", {'n', "num-mappings-for-segment"});
     args::ValueFlag<uint32_t> num_mappings_for_short_seq(parser, "N", "number of mappings to retain for each sequence shorter than segment length [default: 1]", {'S', "num-mappings-for-short-seq"});
     args::Flag skip_self(parser, "", "skip self mappings when the query and target name is the same (for all-vs-all mode)", {'X', "skip-self"});
-    args::ValueFlag<char> skip_prefix(parser, "C", "skip mappings when the query and target have the same prefix before the given character C", {'Y', "skip-prefix"});
+    args::ValueFlag<char> skip_prefix(parser, "C", "skip mappings when the query and target have the same prefix before the last occurrence of the given character C", {'Y', "skip-prefix"});
     args::Flag approx_mapping(parser, "approx-map", "skip base-level alignment, producing an approximate mapping in PAF", {'m',"approx-map"});
     args::Flag no_merge(parser, "no-merge", "don't merge consecutive segment-level mappings", {'M', "no-merge"});
 
