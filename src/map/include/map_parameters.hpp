644c28571baaf7953b2d59550820c6a694584e7f
AndreaGuarracino <andreaguarracino@outlook.com>
2023-09-01 08:38:50 +0200

    fix default identity

--- src/map/include/map_parameters.hpp
+++ src/map/include/map_parameters.hpp
@@ -95,7 +95,7 @@ namespace fixed
 double ss_table_max = 1000.0;                       // Maximum size of dp table for filtering
 double pval_cutoff = 1e-3;                          // p-value cutoff for determining window size
 float confidence_interval = 0.95;                   // Confidence interval to relax jaccard cutoff for mapping (0-1)
-float percentage_identity = 0.85;                   // Percent identity in the mapping step
+float percentage_identity = 0.90;                   // Percent identity in the mapping step
 float ANIDiff = 0.0;                                // Stage 1 ANI diff threshold
 float ANIDiffConf = 0.999;                          // ANI diff confidence
 std::string VERSION = "3.1.1";                      // Version of MashMap
