df6a4155bffa42bb2f78d9187741e6e98fd389f0
Erik Garrison <erik.garrison@gmail.com>
2023-09-05 10:04:41 -0500

    correct hash

--- wfmash.nix
+++ wfmash.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
     owner = "waveygang"; 
     repo = "wfmash";
     rev = "1b63993e8630237cb61f0ef34e380a9eb23702f8";
-    sha256 = "1z54qm5297a363haky8bl2bzi1bdq9k17qgql7fjw5fa06kyqzb7";
+    sha256 = "sha256-KPjge/83CCj+AXAxQmt0Fsbcy7mNnGzH41l9oiebRPQ=";
   };
 
   nativeBuildInputs = [ cmake makeWrapper ];
