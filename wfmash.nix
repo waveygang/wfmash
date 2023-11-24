545db3eda10d4db486e691d52bc558659eb998b5
Erik Garrison <erik.garrison@gmail.com>
2023-11-24 15:15:10 -0600

    nix build v0.12.2

--- wfmash.nix
+++ wfmash.nix
@@ -1,14 +1,14 @@
-{ lib, stdenv, fetchFromGitHub, cmake, gsl, gmp, makeWrapper, jemalloc, htslib, git, zlib, pkgconfig }:
+{ lib, stdenv, fetchFromGitHub, cmake, gsl, gmp, makeWrapper, jemalloc, htslib, git, zlib, pkg-config }:
 
 stdenv.mkDerivation rec {
   pname = "wfmash";
-  version = "0.12.0";
+  version = "0.12.2";
 
   src = fetchFromGitHub {
     owner = "waveygang"; 
     repo = "wfmash";
-    rev = "01f812e5100d302b593bb3f2687635d987a63edd";
-    sha256 = "sha256-X74Bk4+Am/ubhvAD/oH791Dw2QHA4ph2CGsx6sVBu3M=";
+    rev = "180455df2aaa298fbe2dafb1ddfc2796c78b3fcd";
+    sha256 = "sha256-LxmVZcdOYWr1taMuzoW9/7bXJNo5NVgqOCz08gG7CuE=";
   };
 
   nativeBuildInputs = [ cmake makeWrapper ];
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
     htslib
     git
     zlib
-    pkgconfig
+    pkg-config
   ];
 
   postPatch = ''
