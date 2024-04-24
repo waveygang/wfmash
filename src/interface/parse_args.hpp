5d8357299795087788eef18a3c4c26ecab0f34bf
AndreaGuarracino <andreaguarracino@outlook.com>
2024-04-24 16:31:45 -0500

    for the chain_gap, do not go above 20k by default

--- src/interface/parse_args.hpp
+++ src/interface/parse_args.hpp
@@ -84,7 +84,7 @@ void parse_args(int argc,
 	args::ValueFlag<std::string> target_list(mapping_opts, "FILE", "file containing list of target sequence names to use", {'A', "target-list"});
     args::Flag approx_mapping(mapping_opts, "approx-map", "skip base-level alignment, producing an approximate mapping in PAF", {'m',"approx-map"});
     args::Flag no_split(mapping_opts, "no-split", "disable splitting of input sequences during mapping [default: enabled]", {'N',"no-split"});
-    args::ValueFlag<std::string> chain_gap(mapping_opts, "N", "chain mappings closer than this distance in query and target, sets approximate maximum variant length detectable in alignment [default: 20k]", {'c', "chain-gap"});
+    args::ValueFlag<std::string> chain_gap(mapping_opts, "N", "chain mappings closer than this distance in query and target, sets approximate maximum variant length detectable in alignment [default: 4*segment_length, up to 20k]", {'c', "chain-gap"});
     args::Flag drop_low_map_pct_identity(mapping_opts, "K", "drop mappings with estimated identity below --map-pct-id=%", {'K', "drop-low-map-id"});
     args::Flag no_filter(mapping_opts, "MODE", "disable mapping filtering", {'f', "no-filter"});
     args::ValueFlag<double> map_sparsification(mapping_opts, "FACTOR", "keep this fraction of mappings", {'x', "sparsify-mappings"});
@@ -420,8 +420,8 @@ void parse_args(int argc,
         map_parameters.chain_gap = l;
         align_parameters.chain_gap = l;
     } else {
-        map_parameters.chain_gap = 4*map_parameters.segLength;
-        align_parameters.chain_gap = 4*map_parameters.segLength;
+        map_parameters.chain_gap = std::min((int64_t)20000, 4*map_parameters.segLength);
+        align_parameters.chain_gap = std::min((int64_t)20000, 4*map_parameters.segLength);
     }
 
     if (drop_low_map_pct_identity) {
