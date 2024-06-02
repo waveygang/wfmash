a04908b9dac2045826ca695d6f5f13fdecfafd31
Erik Garrison <erik.garrison@gmail.com>
2024-06-01 22:25:17 -0500

    fix easy scoring parameters that work well across diverse alignment problems

--- src/interface/parse_args.hpp
+++ src/interface/parse_args.hpp
@@ -108,14 +108,14 @@ void parse_args(int argc,
                                 {'O', "invert-filtering"});
     args::ValueFlag<uint16_t> wflambda_segment_length(alignment_opts, "N", "wflambda segment length: size (in bp) of segment mapped in hierarchical WFA problem [default: 256]", {'W', "wflamda-segment"});
     args::ValueFlag<std::string> wfa_score_params(alignment_opts, "mismatch,gap1,ext1",
-												  "score parameters for the wfa alignment (affine); match score is fixed at 0 [default: 6,8,1]",
+												  "score parameters for the wfa alignment (affine); match score is fixed at 0 [default: 2,3,1]",
 												  {"wfa-params"});
     args::ValueFlag<std::string> wfa_patching_score_params(alignment_opts, "mismatch,gap1,ext1,gap2,ext2",
-														   "score parameters for the wfa patching alignment (convex); match score is fixed at 0 [default: 5,8,2,49,1]",
+														   "score parameters for the wfa patching alignment (convex); match score is fixed at 0 [default: 3,4,2,24,1]",
 														   {"wfa-patching-params"});
     //wflign parameters
     args::ValueFlag<std::string> wflign_score_params(alignment_opts, "mismatch,gap1,ext1",
-													 "score parameters for the wflign alignment (affine); match score is fixed at 0 [default: 4,6,1]",
+													 "score parameters for the wflign alignment (affine); match score is fixed at 0 [default: 2,3,1]",
 													 {"wflign-params"});
     args::ValueFlag<float> wflign_max_mash_dist(alignment_opts, "N", "maximum mash distance to perform the alignment in a wflambda segment [default: adaptive with respect to the estimated identity]", {'b', "max-mash-dist"});
     args::ValueFlag<int> wflign_min_wavefront_length(alignment_opts, "N", "min wavefront length for heuristic WFlign [default: 1024]", {'j', "wflign-min-wf-len"});
@@ -282,9 +282,9 @@ void parse_args(int argc,
         align_parameters.wfa_gap_opening_score = params[1];
         align_parameters.wfa_gap_extension_score = params[2];
     } else {
-        align_parameters.wfa_mismatch_score = -1;
-        align_parameters.wfa_gap_opening_score = -1;
-        align_parameters.wfa_gap_extension_score = -1;
+        align_parameters.wfa_mismatch_score = 2;
+        align_parameters.wfa_gap_opening_score = 3;
+        align_parameters.wfa_gap_extension_score = 1;
     }
 
     if (!args::get(wfa_patching_score_params).empty()) {
@@ -305,11 +305,11 @@ void parse_args(int argc,
         align_parameters.wfa_patching_gap_opening_score2 = params[3];
         align_parameters.wfa_patching_gap_extension_score2 = params[4];
     } else {
-        align_parameters.wfa_patching_mismatch_score = -1;
-        align_parameters.wfa_patching_gap_opening_score1 = -1;
-        align_parameters.wfa_patching_gap_extension_score1 = -1;
-        align_parameters.wfa_patching_gap_opening_score2 = -1;
-        align_parameters.wfa_patching_gap_extension_score2 = -1;
+        align_parameters.wfa_patching_mismatch_score = 3;
+        align_parameters.wfa_patching_gap_opening_score1 = 4;
+        align_parameters.wfa_patching_gap_extension_score1 = 2;
+        align_parameters.wfa_patching_gap_opening_score2 = 24;
+        align_parameters.wfa_patching_gap_extension_score2 = 1;
     }
 
     if (!args::get(wflign_score_params).empty()) {
@@ -328,9 +328,9 @@ void parse_args(int argc,
         align_parameters.wflign_gap_opening_score = params[1];
         align_parameters.wflign_gap_extension_score = params[2];
     } else {
-        align_parameters.wflign_mismatch_score = -1;
-        align_parameters.wflign_gap_opening_score = -1;
-        align_parameters.wflign_gap_extension_score = -1;
+        align_parameters.wflign_mismatch_score = 2;
+        align_parameters.wflign_gap_opening_score = 3;
+        align_parameters.wflign_gap_extension_score = 1;
     }
 
     if (wflign_max_mash_dist) {
