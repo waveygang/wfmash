2ff7c8b0e2a396e7cdfed1d94a192f65f4986153
AndreaGuarracino <andreaguarracino@outlook.com>
2025-03-06 09:54:22 +0100

    fix: Update script to generate git version with additional source path

--- scripts/make_source_targball.sh
+++ scripts/make_source_targball.sh
@@ -10,7 +10,7 @@ git fetch --tags origin
 LATEST_TAG="$(git describe --tags `git rev-list --tags --max-count=1`)"
 git checkout "${LATEST_TAG}"
 git submodule update --init --recursive
-bash scripts/generate_git_version.sh $PWD/src
+bash scripts/generate_git_version.sh $PWD/src $PWD/src/common/wflign/src
 sed 's/execute_process(COMMAND bash/#execute_process(COMMAND bash/g' CMakeLists.txt -i
 rm -Rf .git
 find src/common -name ".git" -exec rm -Rf "{}" \;
